a901d9424c47cfb92b809bd94758df4732a62f42
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-03-20 14:55:25 +0100

    closest pair | benchmarking support for multiple runs per input

--- closest-pair/ts/src/benchmark.ts
+++ closest-pair/ts/src/benchmark.ts
@@ -2,15 +2,23 @@
 
 export function benchmark<T, R>(
   func: (x: T) => R,
-  inputs: T[]
+  inputs: T[],
+  runs = 1,
 ): Array<[T, number]> {
 
   return inputs.map(input => {
-    const start = Date.now();
-    const result = func(input);
-    const end = Date.now();
+    let totalTime = 0;
 
-    return [input, end - start]
+    for(let i = 0; i < runs; i++){
+      const start = Date.now();
+      const result = func(input);
+      const end = Date.now();
+
+      totalTime += end - start;
+    }
+
+    return [input, totalTime / runs]
   })
 
 }
+
