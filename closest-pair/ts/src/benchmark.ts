3f51ca4fa4af735280aed970d1b12a661dd7ab4e
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-03-20 21:29:53 +0100

    closest pairs | benchmark module refactoring

--- closest-pair/ts/src/benchmark.ts
+++ closest-pair/ts/src/benchmark.ts
@@ -1,24 +1,45 @@
+import { type Point, randomPoints, toDesmosPointList } from "./points";
+import { writeFileSync, readFileSync } from "fs";
 
-
-export function benchmark<T, R>(
-  func: (x: T) => R,
-  inputs: T[],
-  runs = 1,
-): Array<[T, number]> {
-
-  return inputs.map(input => {
-    let totalTime = 0;
-
-    for(let i = 0; i < runs; i++){
-      const start = Date.now();
-      const result = func(input);
-      const end = Date.now();
-
-      totalTime += end - start;
-    }
-
-    return [input, totalTime / runs]
-  })
-
+function buildArr<T>(
+  size: number, 
+  getItem: (i: number, xs: T[]) => T
+): T[] {
+  const rtnArr: T[] = [];
+  for(let i = 0; i < size; i++) rtnArr.push(getItem(i, rtnArr));
+  return rtnArr;
 }
 
+export function createBenchSuite(file: string): void {
+  const inputs = buildArr(100, i => randomPoints(i * 100, [-1000, 1000], [-1000, 1000]));
+  const wstream = writeFileSync(file, JSON.stringify(inputs), { encoding: "utf-8"});
+}
+
+export function loadBenchSuite(file: string): Point[][] {
+  return JSON.parse(readFileSync(file, { encoding: "utf-8" }));
+}
+
+
+export function runBenchmarks(
+  closestPair: (points: Array<Point>) => [Point, Point],
+  benchSuite: Point[][], 
+  file: string
+): void {
+  const benchResults: [number, number][] = benchSuite.map(input => {
+    const start = Date.now();
+    const result = closestPair(input);
+    const end = Date.now();
+
+    return [input.length, end - start]
+  });
+
+
+  writeFileSync(
+    file, 
+    JSON.stringify({
+      nodeArray: benchResults,
+      desmosList: toDesmosPointList(benchResults)
+    }), 
+    { encoding: "utf-8"}
+  );
+}
