704d3b5b212cfba71725cf52659479c82c5d650d
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-03-20 21:32:34 +0100

    closest pairs | implemented quadratic and squared distance optimization

--- closest-pair/ts/src/naive.ts
+++ closest-pair/ts/src/naive.ts
@@ -1,26 +1,121 @@
 
-import { type Point, computeDistance } from "./points.js";
+import { type Point, computeDistance, computeDistanceSquared } from "./points.js";
+import { runBenchmarks, loadBenchSuite } from "./benchmark.js";
+import { runTests } from "./test.js";
 
-// store the current minimum distance (initialize at infinity)
-// compute the distance between ever possible pair of points
-// return the pair with the minimu distance
 
-export function closestPair(points: Array<Point>): [Point, Point] {
-  let pair: [Point, Point] = [ points[0], points[1] ];
-  let minDistance: number = Infinity;
-
-  for(let i = 0; i < points.length; i++){
-    for(let j = 0; j < points.length; j++){
-      if(i == j) continue;
-
-      const distance = computeDistance(points[i], points[j]);
-
-      if(distance < minDistance){
-        minDistance = distance;
-        pair = [ points[i], points[j] ];
+{
+  // VERSION 0
+  function closestPair(points: Array<Point>): [Point, Point] {
+    let pair: [Point, Point] = [ points[0], points[1] ];
+    let minDistance: number = Infinity;
+  
+    for(let i = 0; i < points.length; i++){
+      for(let j = 0; j < points.length; j++){
+        if(i == j) continue;
+  
+        const distance = computeDistance(points[i], points[j]);
+  
+        if(distance < minDistance){
+          minDistance = distance;
+          pair = [ points[i], points[j] ];
+        }
       }
     }
+  
+    return pair;
   }
 
-  return pair;
+  runTests(closestPair);
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite0.json"), 
+    "./bench_results/naive_v0/suite0.json"
+  );
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite1.json"), 
+    "./bench_results/naive_v0/suite1.json"
+  );
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite2.json"), 
+    "./bench_results/naive_v0/suite2.json"
+  );
+}
+
+{
+  // VERSION 1
+  function closestPair(points: Array<Point>): [Point, Point] {
+    let pair: [Point, Point] = [ points[0], points[1] ];
+    let minDistance: number = Infinity;
+  
+    for(let i = 0; i < points.length; i++){
+      for(let j = i + 1; j < points.length; j++){
+        const distance = computeDistance(points[i], points[j]);
+  
+        if(distance < minDistance){
+          minDistance = distance;
+          pair = [ points[i], points[j] ];
+        }
+      }
+    }
+  
+    return pair;
+  }
+
+  runTests(closestPair);
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite0.json"), 
+    "./bench_results/naive_v1/suite0.json"
+  );
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite1.json"), 
+    "./bench_results/naive_v1/suite1.json"
+  );
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite2.json"), 
+    "./bench_results/naive_v1/suite2.json"
+  );
+}
+
+{
+  // VERSION 2
+  function closestPair(points: Array<Point>): [Point, Point] {
+    let pair: [Point, Point] = [ points[0], points[1] ];
+    let minDistance: number = Infinity;
+  
+    for(let i = 0; i < points.length; i++){
+      for(let j = i + 1; j < points.length; j++){
+        const distance = computeDistanceSquared(points[i], points[j]);
+  
+        if(distance < minDistance){
+          minDistance = distance;
+          pair = [ points[i], points[j] ];
+        }
+      }
+    }
+  
+    return pair;
+  }
+
+  runTests(closestPair);
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite0.json"), 
+    "./bench_results/naive_v2/suite0.json"
+  );
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite1.json"), 
+    "./bench_results/naive_v2/suite1.json"
+  );
+  runBenchmarks(
+    closestPair, 
+    loadBenchSuite("./bench_suites/suite2.json"), 
+    "./bench_results/naive_v2/suite2.json"
+  );
 }
