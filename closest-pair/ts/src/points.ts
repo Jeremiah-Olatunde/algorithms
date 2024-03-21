568ec9e2aaff4274fecd70067ffc8ba93b7f582d
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-03-22 00:49:32 +0100

    closest pair | points module refactoring

--- closest-pair/ts/src/points.ts
+++ closest-pair/ts/src/points.ts
@@ -1,13 +1,6 @@
-export type Point = [number, number];
+import { buildArr } from "./array.js";
 
-function buildArr<T>(
-  size: number, 
-  getItem: (i: number, xs: T[]) => T
-): T[] {
-  const rtnArr: T[] = [];
-  for(let i = 0; i < size; i++) rtnArr.push(getItem(i, rtnArr));
-  return rtnArr;
-}
+export type Point = [number, number];
 
 function randomInt(lowerBound: number, upperBound: number): number {
   return Math.floor(Math.random() * (upperBound - lowerBound)) + lowerBound;
@@ -29,12 +22,6 @@ export function randomPoints(
   yRange: [number, number]
 ): Array<Point> {
   return buildArr(size, _ => randomPoint(xRange, yRange))
-}
-
-export function computeDistance(p: Point, q: Point): number {
-  return Math.sqrt(
-    Math.pow(q[0] - p[0], 2) + Math.pow(q[1] - p[1], 2)
-  );
 }
 
 export function computeDistanceSquared(p: Point, q: Point): number {
