c55fd966fe3395210350541a0b65b0db11d452fd
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2024-03-22 01:40:51 +0100

    closest pair | benchmark refactoring and benching naive and divide and conquer results

--- closest-pair/ts/src/benchmark.ts
+++ closest-pair/ts/src/benchmark.ts
@@ -1,45 +1,36 @@
-import { type Point, randomPoints, toDesmosPointList } from "./points";
-import { writeFileSync, readFileSync } from "fs";
+import { createWriteStream } from "fs";
 
-function buildArr<T>(
-  size: number, 
-  getItem: (i: number, xs: T[]) => T
-): T[] {
-  const rtnArr: T[] = [];
-  for(let i = 0; i < size; i++) rtnArr.push(getItem(i, rtnArr));
-  return rtnArr;
+import { buildArr } from "./array.js";
+import { randomPoints, type Point, toDesmosPointList } from "./points.js";
+
+import * as naive from "./naive.js";
+import * as divNCqr from "./div_and_cqr.js";
+
+
+function time<T, R>(f: (x: T) => R, input: T): number {
+  const start = Date.now();
+  const result = f(input);
+  return Date.now() - start;
 }
 
-export function createBenchSuite(file: string): void {
-  const inputs = buildArr(100, i => randomPoints(i * 100, [-1000, 1000], [-1000, 1000]));
-  const wstream = writeFileSync(file, JSON.stringify(inputs), { encoding: "utf-8"});
-}
+const suite = buildArr(100, i => {
+    const pointCount = (i + 1) * 100;
+    const range = 1_000_000;
+    return [
+      pointCount, 
+      randomPoints(pointCount, [-range, range], [-range, range])
+    ] as [number, Point[]]
+  }
+);
 
-export function loadBenchSuite(file: string): Point[][] {
-  return JSON.parse(readFileSync(file, { encoding: "utf-8" }));
-}
+const naiveResults: [number, number][] = suite.map(([n, test]) => [n, time(naive.cp, test)]);
+const divNCqrResults: [number, number][] = suite.map(([n, test]) => [n, time(divNCqr.cp, test)]);
 
-
-export function runBenchmarks(
-  closestPair: (points: Array<Point>) => [Point, Point],
-  benchSuite: Point[][], 
-  file: string
-): void {
-  const benchResults: [number, number][] = benchSuite.map(input => {
-    const start = Date.now();
-    const result = closestPair(input);
-    const end = Date.now();
-
-    return [input.length, end - start]
-  });
+const wstream = createWriteStream("./benchmarks/results.txt", { encoding: "utf-8", flags: "a" });
 
 
-  writeFileSync(
-    file, 
-    JSON.stringify({
-      nodeArray: benchResults,
-      desmosList: toDesmosPointList(benchResults)
-    }), 
-    { encoding: "utf-8"}
-  );
-}
+
+wstream.write(new Date().toString() + "\n");
+wstream.write("NAIVE => " + toDesmosPointList(naiveResults) + "\n");
+wstream.write("DIVIDE N CONQUER => " + toDesmosPointList(divNCqrResults) + "\n");
+wstream.write("\n");
